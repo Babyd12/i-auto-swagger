b56b4c0310a4a09183d55bcfec2b62a8451cc666
Babyd12 <mamadcir24@gmail.com>
2023-12-27 05:17:55 +0000

    10

--- src/Commands/InstallSwagger.php
+++ src/Commands/InstallSwagger.php
@@ -6,16 +6,16 @@ use Composer\Script\Event;
 
 class InstallSwagger
 {
-    public static function handle(Event $event)
+    public static function handle()
     {
-        $io = $event->getIO();
-        $io->write('Installing Swagger...');
+        // $io = $event->getIO();
+        // $io->write('Installing Swagger...');
 
         // Commandes d'installation de Swagger
-        passthru('composer require darkaonline/l5-swagger');
-        passthru('php artisan vendor:publish --provider="L5Swagger\L5SwaggerServiceProvider"');
-        passthru('php artisan l5-swagger:generate');
+        exec('composer require darkaonline/l5-swagger');
+        exec('php artisan vendor:publish --provider="L5Swagger\L5SwaggerServiceProvider"');
+        exec('php artisan l5-swagger:generate');
 
-        $io->write('Swagger installed successfully.');
+        // $io->write('Swagger installed successfully.');
     }
 }
