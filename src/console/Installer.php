ff65e2485281194e0527a925183ade3b97ad0dfb
Babyd12 <mamadcir24@gmail.com>
2023-12-28 00:18:57 +0000

    25

---
+++ src/console/Installer.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Mcire\L5AutoSwagger\Console;
+
+class Installer
+{
+    public static function installSwagger()
+    {
+        echo "Installing Swagger...\n";
+        echo "Bonjour tout le monde depuis l'installateur !\n";
+        // Composer require
+        // system('composer require darkaonline/l5-swagger');
+
+        // // Artisan commands
+        // system('php artisan vendor:publish --provider="Tymon\JWTAuth\Providers\LaravelServiceProvider"');
+        // system('php artisan jwt:secret');
+        // system('php artisan vendor:publish --provider "L5Swagger\L5SwaggerServiceProvider"');
+
+        // echo "Swagger installed successfully!\n";
+    }
+}
